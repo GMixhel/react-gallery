521117030082c97c4d3b87bf60d154c9aee77b0a
John Doe <gmra9329@gmail.com>
2023-08-23 08:33:34 +0200

    new update

--- src/data/images.js
+++ src/data/images.js
@@ -47,4 +47,60 @@ export const images = [
     path: "../resources/wine-bottle-4324050_1920.jpg",
     description: "Degustation",
   },
+  {
+    id: "brittani-burns-cwp4qGNT4W4",
+    name: "brittani-burns",
+    path: "'../resources/brittani-burns-cwp4qGNT4W4.jpg'",
+    description: "autentic bar",
+  },
+  {
+    id: "mathias-reding-DjVjNCsRQ1Q",
+    name: "reding books",
+    path: "../resources/mathias-reding-DjVjNCsRQ1Q.jpg",
+    description: "mathias-reding",
+  },
+  {
+    id: "mathias-reding-3erG8IDvJZk",
+    name: "lucas-reding",
+    path: "../resources/mathias-reding-3erG8IDvJZk.jpg",
+    description: "vision fire",
+  },
+  {
+    id: "markus-spiske-IRtkFE20NgQ",
+    name: "illusion 0.2",
+    path: "../resources/markus-spiske-IRtkFE20NgQ.jpg",
+    description: "markus-spiske",
+  },
+  {
+    id: "mohammadreza-babaei-4MkSCroXEAU",
+    name: "babaei lies",
+    path: "../resources/mohammadreza-babaei-4MkSCroXEAU.jpg",
+    description: "mohammadreza-babaei",
+  },
+  {
+    id: "annie-spratt-RP4zgMu3CbE",
+    name: "behind the wall",
+    path: "../resources/annie-spratt-RP4zgMu3CbE.jpg",
+    description: "annie-spratt",
+  },
+  {
+    id: "febiyan-TcRS867Pw3A",
+    name: "wing",
+    path: "../resources/febiyan-TcRS867Pw3A.jpg",
+    description: "nati-melnychuk",
+  },
+  {
+    id: "natalie-behn-qj_zfYm-sdI",
+    name: "old travel in time",
+    path: "'../resources/natalie-behn-qj_zfYm-sdI.jpg'",
+    description: "natalie-behn",
+  },
+  {
+    id: "caden-tormey-omgls1QAOtU",
+    name: "white machine",
+    path: "../resources/caden-tormey-omgls1QAOtU.jpg",
+    description: "caden-tormey",
+  },
 ];
+
+
